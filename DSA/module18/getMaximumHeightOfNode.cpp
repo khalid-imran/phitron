6dd6fba28ef68ee710c57b5aeeeec52c59d33c8c
Khalid Imran <ekhalid090@gmail.com>
2024-09-21 07:58:47 +0600

    hight of a binary tree

---
+++ DSA/module18/getMaximumHeightOfNode.cpp
@@ -0,0 +1,69 @@
+// how to take input of binary tree;
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+    public:
+        int val;
+        Node* left;
+        Node* right;
+    Node(int val) {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* inputTree() {
+    int val;
+    cin>>val;
+    Node* root;
+    if (val == -1) {
+        root = NULL;
+    } else {
+        root = new Node(val);
+    }
+    queue<Node*> q;
+     if (root) {
+        q.push(root);
+    }
+    while(!q.empty()) {
+        Node* f = q.front();
+        q.pop();
+        int left, right;
+        cin>>left>>right;
+
+        Node* leftVal;
+        Node* rightVal;
+        if (left == -1) {
+            leftVal = NULL;
+        } else {
+            leftVal = new Node(left);
+        }
+         if (right == -1) {
+            rightVal = NULL;
+        } else {
+            rightVal = new Node(right);
+        }
+        f->left = leftVal;
+        f->right = rightVal;
+
+        if(f->left) {
+            q.push(f->left);
+        }
+        if(f->right) {
+            q.push(f->right);
+        }
+    }
+    return root;
+}
+int maxHeight(Node* root) {
+    if (root == NULL) return 0;
+    int leftCount = maxHeight(root->left);
+    int rightCount = maxHeight(root->right);
+    return max(leftCount, rightCount) + 1;
+}
+int main() {
+    Node* root = inputTree();
+    int count = maxHeight(root);
+    cout << count << endl;
+    return 0;
+}
